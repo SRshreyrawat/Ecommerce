63a175902dfcbc589288542f7bd54ac2edffb09d
Shrey Rawat <shreyrawat01928@gmail.com>
2026-01-24 12:35:55 +0530

    OrderDTO

--- src/main/java/com/project/Ecommerce/Dto/OrderDTO.java
+++ src/main/java/com/project/Ecommerce/Dto/OrderDTO.java
@@ -1,5 +1,23 @@
 package com.project.Ecommerce.Dto;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.project.Ecommerce.Entity.OrderItem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDTO {
-    
+    private Long orderId;
+    private String email;
+    private List<OrderItem> orderitems;
+    private LocalDate orderDate;
+    private PaymentDTO payment;
+    private double totalAmount;
+    private String orderStatus;
+    private Long addressId;
 }
