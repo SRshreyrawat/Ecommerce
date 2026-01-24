b3c668c523948dd855a81886cfb7fbff6dee1864
Shrey Rawat <shreyrawat01928@gmail.com>
2026-01-24 12:29:33 +0530

    PaymentDTO

--- src/main/java/com/project/Ecommerce/Dto/PaymentDTO.java
+++ src/main/java/com/project/Ecommerce/Dto/PaymentDTO.java
@@ -1,5 +1,20 @@
 package com.project.Ecommerce.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentDTO {
-    
+
+
+   private Long paymentId;
+   private String paymentMethod;
+   private String pgPaymentId;
+   private String pgStatus;
+   private String PgResponseMessage;
+   private String pgname;
+
 }
