4333e46c58ae919488b35217e990d02af72e2964
Shrey Rawat <shreyrawat01928@gmail.com>
2026-01-24 12:32:06 +0530

    OrderItemDTO

--- src/main/java/com/project/Ecommerce/Dto/OrderItemDTO.java
+++ src/main/java/com/project/Ecommerce/Dto/OrderItemDTO.java
@@ -1,5 +1,17 @@
 package com.project.Ecommerce.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemDTO {
+    private Long orderitemId;
+    private ProductDTO product;
+    private Integer quantity;
+    private double discount;
+    private double orderedProductPrice;
     
 }
