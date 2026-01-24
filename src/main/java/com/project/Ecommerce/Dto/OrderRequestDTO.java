412faabe9284f87ddc631d443eb2fa055adc3565
Shrey Rawat <shreyrawat01928@gmail.com>
2026-01-24 12:39:35 +0530

    OrderRequestDTO

--- src/main/java/com/project/Ecommerce/Dto/OrderRequestDTO.java
+++ src/main/java/com/project/Ecommerce/Dto/OrderRequestDTO.java
@@ -1,5 +1,16 @@
 package com.project.Ecommerce.Dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderRequestDTO {
-    
+    private Long addressId;
+    private Long paymentMethod;
+    private String pgName;
+    private String pgPaymentId;
+    private String pgStatus;
+    private String pgResponseMessage;
 }
